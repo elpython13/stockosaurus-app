29259f89b95b3705932f0072976e378712450fd3
Kristian Ellis <kristian.w.ellis@gmail.com>
2021-04-22 01:56:28 +0100

    updating stylings, added close and high

--- app/src/components/LineSimple.js
+++ app/src/components/LineSimple.js
@@ -3,37 +3,30 @@ import {
   LineChart,
   Line,
   XAxis,
-  YAxis,
+  YAxis,   
   CartesianGrid,
   Tooltip,
-  Legend
+  Legend,
+  ResponsiveContainer
 } from "recharts";
 
 
 export default function LineSimple(props) {
   return (
-    <LineChart
-      width={500}
-      height={300}
-      data={props.data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="low"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-      />
-    </LineChart>
+      <ResponsiveContainer width="90%" height="90%">
+        <LineChart width={500} height={300} data={props.data}
+          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+          <XAxis dataKey="date" domain={['auto', 'auto']} />
+          <YAxis type="number" domain={['auto', 'auto']}/>
+          <Tooltip animationDuration='500' />
+          <Legend />
+          <Line type="monotone" dataKey="low" stroke="#8884d8" 
+            activeDot={{ r: 6 }} legendType="circle" dot={false}/>
+          <Line type="monotone" dataKey="high" stroke="cyan" 
+            activeDot={{ r: 6 }} legendType="circle" dot={false}/>
+          <Line type="monotone" dataKey="close" stroke="pink" 
+            activeDot={{ r: 6 }} legendType="circle" dot={false}/>
+        </LineChart>
+      </ResponsiveContainer>
   );
 }
