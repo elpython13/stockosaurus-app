8b13eb164a0189ecb02171e93419051404e83e7b
Kristian Ellis <kristian.w.ellis@gmail.com>
2021-05-01 21:46:50 +0100

    removed news

--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
-const NavBar = () => (
+export default function NavBar (){
+    return (
     <div className="navbar-container">
         <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="./">stockosaurus</Navbar.Brand>
             <Nav className="mr-auto">
             <Nav.Link href="./stocks">stocks</Nav.Link>
-            <Nav.Link href="./">news</Nav.Link>
-            <Nav.Link href="./">architecture</Nav.Link>
+            <Nav.Link href="./architecture">architecture</Nav.Link>
             </Nav>
         </Navbar>
-   </div>
-);
-
-export default NavBar
+    </div>
+    );
+}
